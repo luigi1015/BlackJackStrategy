20f0f92ca8cac44e5d95c07b7ddef3a55a41a610
luigi1015 <github@314ghz.com>
2014-03-31 16:11:31 -0500

    Did some work to get the Player class ready for an iterator class over the hand(s) it has.

--- Player.h
+++ Player.h
@@ -2,16 +2,19 @@
 #define Player_h
 
 #include "Hand.h"
+#include <vector>
 
 namespace Blackjack
 {
 	class Player
 	{//A Blackjack player.
 		private:
-			Hand hand;
+			std::vector<Hand> hands;//Put the hand in a vector since there could be more than one due to a split.
 
 		public:
 			Player();//Default Cunstructor.
+			size_t numHands() const;//Returns the number of hands the player currently has.
+			Hand& getHand( size_t n );//Returns the hand at location n. n should be between 0 and numHands()-1, inclusive.
 	};
 }
 
