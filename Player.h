5508b2c34cd46fbf6f262b0d315065e1a4ce6321
luigi1015 <github@314ghz.com>
2014-04-03 08:16:10 -0500

    Added a method definition for splitting a player's hand.

--- Player.h
+++ Player.h
@@ -15,6 +15,7 @@ namespace Blackjack
 			Player();//Default Cunstructor.
 			size_t numHands() const;//Returns the number of hands the player currently has.
 			Hand& getHand( size_t n );//Returns the hand at location n. n should be between 0 and numHands()-1, inclusive.
+			void split( size_t n );//Splits the hand a location n if possible.
 	};
 }
 
