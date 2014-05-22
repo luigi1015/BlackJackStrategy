d157446a8037bc10c343bc02af7474a8f7229dab
luigi1015 <github@314ghz.com>
2014-05-22 12:30:09 -0500

    Added dealer.cpp

---
+++ Dealer.cpp
@@ -0,0 +1,47 @@
+#include "Dealer.h"
+
+/*
+namespace Blackjack
+{
+	class Dealer: public Player
+	{//A Blackjack dealer.
+		private:
+			std::vector<Player*> players;
+
+		public:
+			Dealer();//Default Constructor.
+			~Dealer();//Default Destructor. Frees all the player pointers.
+			//void deal();//Deal cards to players.
+			void startGame();//Starts the players playing.
+			void addUserPlayer( std::string newName );//Registers a new user's player for the game. newName is the name of the new player.
+			void addAutoPlayer( std::string newName );//Registers a new automatic player for the game. newName is the name of the new player.
+			Card getRandomCard();//Returns a randomly valued card.
+	};
+}
+*/
+namespace Blackjack
+{
+	Dealer()
+	{//Default Constructor.
+	}
+
+	~Dealer()
+	{//Default Destructor. Frees all the player pointers.
+	}
+
+	void startGame()
+	{//Starts the players playing.
+	}
+
+	void addUserPlayer( std::string newName )
+	{//Registers a new user's player for the game. newName is the name of the new player.
+	}
+
+	void addAutoPlayer( std::string newName )
+	{//Registers a new automatic player for the game. newName is the name of the new player.
+	}
+
+	Card getRandomCard()
+	{//Returns a randomly valued card.
+	}
+}
