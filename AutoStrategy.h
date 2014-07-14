329cbf4c8ab196b13d87468673b331e6500acdef
luigi1015 <github@314ghz.com>
2014-07-14 17:33:16 -0500

    Added AutoStrategy class.

---
+++ AutoStrategy.h
@@ -0,0 +1,18 @@
+#ifndef AutoStrategy_h
+#define AutoStrategy_h
+
+#include <vector>
+
+namespace Blackjack
+{
+	class AutoStrategy
+	{//A Blackjack player that automatically plays without user input.
+		private:
+
+		public:
+			enum Decision { Bet, Stand, DoubleDown, Split );
+			virtual int decide( Card dealerCard, HandCollection playerCards ) const;//Takes the dealer's card, the player's cards, decide what to do, and return the appropriate Decision value.
+	};
+}
+
+#endif /* AutoStrategy_h */
